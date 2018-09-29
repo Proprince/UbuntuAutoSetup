9fdbe99bcfd3fdbfb67fe8b43698e7c59c0bd550
Proprince <zrp1017@163.com>
2018-09-29 08:53:28 +0800

    -

--- term_tools.sh
+++ term_tools.sh
@@ -42,15 +42,17 @@ git config --global user.email xxx
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sudo sh
 sudo apt-get install -y autojump
 
+pip3 install thefuck
+
 # settings for vim, zsh, tmux
 ln ubuntu_setup/vimrc -f $HOME/.vimrc
-ln ubuntu_setup/zshrc -f $HOME/.vimrc
+ln ubuntu_setup/zshrc -f $HOME/.zshrc
 ln ubuntu_setup/tmux.conf -f $HOME/.tmux.conf
 
 # vim setting
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 mkdir $HOME/.vim/colors # color schemes
 ln ubuntu_setup/jellybeans.vim $HOME/.vim/colors
-vim +PluginInstall
+vim +PluginInstall +qall
 $HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer
-ln ycm_extra_conf.py $/HOME/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
+ln ubuntu_setup/ycm_extra_conf.py $/HOME/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
