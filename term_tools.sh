bb8aee0e4b307a2d8c8b10f54a4b9234b21fc4b6
Proprince <zrp1017@163.com>
2018-09-29 11:04:55 +0800

    warning: ycm

--- term_tools.sh
+++ term_tools.sh
@@ -45,17 +45,22 @@ git config --global user.email xxx
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sudo sh
 sudo apt-get install -y autojump
 
-pip3 install thefuck
+sudo pip3 install thefuck
 
 # settings for vim, zsh, tmux
 ln ubuntu_setup/vimrc -f $HOME/.vimrc
 ln ubuntu_setup/zshrc -f $HOME/.zshrc
 ln ubuntu_setup/tmux.conf -f $HOME/.tmux.conf
 
+sudo chsh -s /bin/zsh
+
 # vim setting
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 mkdir $HOME/.vim/colors # color schemes
 ln ubuntu_setup/jellybeans.vim $HOME/.vim/colors
 vim +PluginInstall +qall
+
+# if RAM is less than 1.5GB, can't use ycm
+# need to delete settings in vimrc
 $HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer
-ln ubuntu_setup/ycm_extra_conf.py $/HOME/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
+ln -f ubuntu_setup/ycm_extra_conf.py $/HOME/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
